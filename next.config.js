4a38e495fdd93ebd055efdee77c3ebf29ea869be
oskarvitko <oskarvitko16@gmail.com>
2026-04-10 15:56:04 +0200

    try to deploy

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
+                protocol: 'http',
                 hostname: 'api.stroisad.by',
             },
             {
